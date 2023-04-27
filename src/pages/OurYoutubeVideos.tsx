955339144a4efdbe0a6ddfeb3dad41a80b60fdae
jmlinger <jmlinger10@gmail.com>
2023-04-27 14:40:37 -0300

    feat: it adds youtube videos to cards.

--- src/pages/OurYoutubeVideos.tsx
+++ src/pages/OurYoutubeVideos.tsx
@@ -13,66 +13,46 @@ const OurYoutubeVideos = () => {
               {/* <h5 className='text-base mb:text-lg text-amber-700 mb-1'>See Our recent videos</h5> */}
               <h1 className='text-4xl md:text-6xl text-gray-700 font-semibold'>See Our recent videos</h1>
             </div>
-            <div className='flex flex-wrap -m-4'>
-              <div className='p-4 sm:w-1/2 lg:w-1/3'>
+            <div className='flex flex-wrap justify-center'>
+              <div className='p-4 md:w-1/2 lg:w-1/3'>
                 <div className='h-full border-2 border-gray-200 border-opacity-60 rounded-lg overflow-hidden'>
-                  <Image
-                    alt="Pictures of the class"
-                    src={hotel}
-                    className='lg:h-72 md:h-48 w-full object-cover object-center' //h-72 para 54
-                  />
+                  <iframe className='w-full aspect-video' src="https://www.youtube.com/embed/xNRJwmlRBNU" title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen></iframe>
                   <div className='p-6 hover:bg-amber-700 hover:text-white transition duration-300 ease-in cursor-pointer'>
                     <h2 className='text-base font-medium text-amber-300 mb-1'>October 21, 2020</h2>
                     <h1 className='text-2xl font-semibold mb-3'>Beautiful Beach</h1>
                     <p className='leadind-relaxed mb-3'> Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
                   </div>
                 </div>
-
               </div>
-              <div className='p-4 sm:w-1/2 lg:w-1/3'>
+              <div className='p-4 md:w-1/2 lg:w-1/3'>
                 <div className='h-full border-2 border-gray-200 border-opacity-60 rounded-lg overflow-hidden'>
-                  <Image
-                    alt="Pictures of the class"
-                    src={hotel}
-                    className='lg:h-72 md:h-48 w-full object-cover object-center' //h-72 para 54
-                  />
+                  <iframe className='w-full aspect-video' src="https://www.youtube.com/embed/xNRJwmlRBNU" title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen></iframe>
                   <div className='p-6 hover:bg-amber-700 hover:text-white transition duration-300 ease-in cursor-pointer'>
                     <h2 className='text-base font-medium text-amber-300 mb-1'>October 21, 2020</h2>
                     <h1 className='text-2xl font-semibold mb-3'>Beautiful Beach</h1>
                     <p className='leadind-relaxed mb-3'> Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
                   </div>
                 </div>
-
               </div>
-              <div className='p-4 sm:w-1/2 lg:w-1/3'>
+              <div className='p-4 md:w-1/2 lg:w-1/3'>
                 <div className='h-full border-2 border-gray-200 border-opacity-60 rounded-lg overflow-hidden'>
-                  <Image
-                    alt="Pictures of the class"
-                    src={hotel}
-                    className='lg:h-72 md:h-48 w-full object-cover object-center' //h-72 para 54
-                  />
+                  <iframe className='w-full aspect-video' src="https://www.youtube.com/embed/xNRJwmlRBNU" title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen></iframe>
                   <div className='p-6 hover:bg-amber-700 hover:text-white transition duration-300 ease-in cursor-pointer'>
                     <h2 className='text-base font-medium text-amber-300 mb-1'>October 21, 2020</h2>
                     <h1 className='text-2xl font-semibold mb-3'>Beautiful Beach</h1>
                     <p className='leadind-relaxed mb-3'> Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
                   </div>
                 </div>
-
               </div>
-              <div className='p-4 sm:w-1/2 lg:w-1/3'>
+              <div className='p-4 md:w-1/2 lg:w-1/3'>
                 <div className='h-full border-2 border-gray-200 border-opacity-60 rounded-lg overflow-hidden'>
-                  <Image
-                    alt="Pictures of the class"
-                    src={hotel}
-                    className='lg:h-72 md:h-48 w-full object-cover object-center' //h-72 para 54
-                  />
+                  <iframe className='w-full aspect-video' src="https://www.youtube.com/embed/xNRJwmlRBNU" title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen></iframe>
                   <div className='p-6 hover:bg-amber-700 hover:text-white transition duration-300 ease-in cursor-pointer'>
                     <h2 className='text-base font-medium text-amber-300 mb-1'>October 21, 2020</h2>
                     <h1 className='text-2xl font-semibold mb-3'>Beautiful Beach</h1>
                     <p className='leadind-relaxed mb-3'> Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
                   </div>
                 </div>
-
               </div>
             </div>
           </div>
