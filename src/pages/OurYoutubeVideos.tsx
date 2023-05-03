56115d9e3a7b20bcd17d1669c9986190c5a95acf
NataliaSRiber <nataliaribeiro@unifei.edu.br>
2023-05-03 12:20:59 -0300

    it modifies cards background color

--- src/pages/OurYoutubeVideos.tsx
+++ src/pages/OurYoutubeVideos.tsx
@@ -17,8 +17,8 @@ const OurYoutubeVideos = () => {
               <div className='p-4 md:w-1/2 lg:w-1/3'>
                 <div className='h-full border-2 border-gray-200 border-opacity-60 rounded-lg overflow-hidden'>
                   <iframe className='w-full aspect-video' src="https://www.youtube.com/embed/xNRJwmlRBNU" title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen></iframe>
-                  <div className='p-6 hover:bg-amber-700 hover:text-white transition duration-300 ease-in cursor-pointer'>
-                    <h2 className='text-base font-medium text-amber-300 mb-1'>October 21, 2020</h2>
+                  <div className='p-6 hover:bg-blue-900 hover:text-white transition duration-300 ease-in cursor-pointer'>
+                    <h2 className='text-base font-medium text-blue-300 mb-1'>October 21, 2020</h2>
                     <h1 className='text-2xl font-semibold mb-3'>Beautiful Beach</h1>
                     <p className='leadind-relaxed mb-3'> Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
                   </div>
@@ -27,8 +27,8 @@ const OurYoutubeVideos = () => {
               <div className='p-4 md:w-1/2 lg:w-1/3'>
                 <div className='h-full border-2 border-gray-200 border-opacity-60 rounded-lg overflow-hidden'>
                   <iframe className='w-full aspect-video' src="https://www.youtube.com/embed/xNRJwmlRBNU" title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen></iframe>
-                  <div className='p-6 hover:bg-amber-700 hover:text-white transition duration-300 ease-in cursor-pointer'>
-                    <h2 className='text-base font-medium text-amber-300 mb-1'>October 21, 2020</h2>
+                  <div className='p-6 hover:bg-blue-900 hover:text-white transition duration-300 ease-in cursor-pointer'>
+                    <h2 className='text-base font-medium text-blue-300 mb-1'>October 21, 2020</h2>
                     <h1 className='text-2xl font-semibold mb-3'>Beautiful Beach</h1>
                     <p className='leadind-relaxed mb-3'> Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
                   </div>
@@ -37,8 +37,8 @@ const OurYoutubeVideos = () => {
               <div className='p-4 md:w-1/2 lg:w-1/3'>
                 <div className='h-full border-2 border-gray-200 border-opacity-60 rounded-lg overflow-hidden'>
                   <iframe className='w-full aspect-video' src="https://www.youtube.com/embed/xNRJwmlRBNU" title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen></iframe>
-                  <div className='p-6 hover:bg-amber-700 hover:text-white transition duration-300 ease-in cursor-pointer'>
-                    <h2 className='text-base font-medium text-amber-300 mb-1'>October 21, 2020</h2>
+                  <div className='p-6 hover:bg-blue-900 hover:text-white transition duration-300 ease-in cursor-pointer'>
+                    <h2 className='text-base font-medium text-blue-300 mb-1'>October 21, 2020</h2>
                     <h1 className='text-2xl font-semibold mb-3'>Beautiful Beach</h1>
                     <p className='leadind-relaxed mb-3'> Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
                   </div>
@@ -47,8 +47,8 @@ const OurYoutubeVideos = () => {
               <div className='p-4 md:w-1/2 lg:w-1/3'>
                 <div className='h-full border-2 border-gray-200 border-opacity-60 rounded-lg overflow-hidden'>
                   <iframe className='w-full aspect-video' src="https://www.youtube.com/embed/xNRJwmlRBNU" title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen></iframe>
-                  <div className='p-6 hover:bg-amber-700 hover:text-white transition duration-300 ease-in cursor-pointer'>
-                    <h2 className='text-base font-medium text-amber-300 mb-1'>October 21, 2020</h2>
+                  <div className='p-6 hover:bg-blue-900 hover:text-white transition duration-300 ease-in cursor-pointer'>
+                    <h2 className='text-base font-medium text-blue-300 mb-1'>October 21, 2020</h2>
                     <h1 className='text-2xl font-semibold mb-3'>Beautiful Beach</h1>
                     <p className='leadind-relaxed mb-3'> Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
                   </div>
