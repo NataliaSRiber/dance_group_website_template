eeeca18812c4889ee1b0a334a0dcebe5efbdae69
NataliaSRiber <nataliaribeiro@unifei.edu.br>
2023-05-03 12:19:14 -0300

    it modifies Aboutpage

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import Image from 'next/image'
-import dance from './assets/dance.jpg'
+import woman from './assets/woman.jpg'
 import Nav from './Nav';
 import WhatsappIcon from './Whatsappicon';
 
@@ -8,16 +8,21 @@ const AboutUs = () => {
     return (
       <>
         <Nav />
-        <div className='z-[-2]'>
           {/* <div className="bg-indigo-600 text-white font-[Poppins] hover:bg-indigo-400 duration-500 w-full h-64">
           </div> */}
+        <div className='w-full relative text-center h-1/3'>
           <Image 
-            className='w-full h-96'
-            src={dance}
+            className='w-full h-full brightness-50'
+            src={woman}
             alt="Picture of the author"
           />
-          <div className='flex flex-col py-20:px-20 md:px-5'>
-            <h1 className='text-3xl md:text-5xl text-gray-700 font-semibold text-center '>About us?</h1>
+          <div className='text-white absolute top-1/2 w-full'>
+            <h1 className='tracking-wide font-semibold md:pb-2 text-md md:text-4xl'>Balanço das Ondas</h1>
+            <p className='font-light text-xs md:text-xl'>Grupo de dança de Balneário Camboriú, Santa Catarina</p>
+          </div>
+          </div>
+          <div className='flex flex-col py-20 md:px-20 px-10'>
+            <h1 className='text-3xl md:text-5xl text-gray-700 font-semibold text-center pb-20'>About us</h1>
             <p className='text-xl text-justify'>
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec sollicitudin, velit a luctus vulputate, orci erat consectetur neque, quis dignissim erat ligula at lacus. Quisque nec purus nec augue tristique rutrum ac imperdiet mi. Nulla consectetur risus vel ante venenatis, eu fermentum lorem placerat. Nunc viverra lobortis mauris, id rhoncus elit rutrum quis. Praesent feugiat finibus enim, sed tempor tortor dictum in. Sed eros magna, pulvinar id mollis in, ornare ac nunc. Donec non sagittis felis. Fusce congue quam velit. Vivamus non elit risus. Duis ante nibh, porttitor in ipsum a, luctus dapibus lectus. Praesent facilisis, felis et viverra vehicula, metus turpis accumsan velit, ut sollicitudin risus mauris non nisl. Mauris mattis lobortis mauris ut varius. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent at vehicula est. Proin egestas, nisl eu semper aliquet, libero quam aliquam nunc, nec rhoncus mi nibh ut nunc.
 
@@ -30,7 +35,6 @@ const AboutUs = () => {
                 Etiam hendrerit ante dui, in finibus diam sollicitudin ac. Fusce lobortis orci ligula, quis molestie arcu hendrerit nec. Duis eu nulla vel mi fringilla molestie vel id massa. Curabitur ut malesuada dui. Duis pretium lacus id dui sagittis pretium. Ut accumsan quam a risus tempor, interdum pellentesque erat pulvinar. Nunc massa neque, mollis in congue id, scelerisque at ipsum. Nam aliquam tellus non ligula mollis cursus. In vel molestie nulla, non mattis turpis. Nunc sed urna ac lacus ultricies aliquam sodales nec massa.
             </p>
           </div>
-        </div>
         <WhatsappIcon />
       </>
     );
