6eed02608b53202d206533fd6b534e658e890558
NataliaSRiber <nataliaribeiro@unifei.edu.br>
2023-05-04 12:31:19 -0300

    feat: it adds font style

--- src/pages/Button.tsx
+++ src/pages/Button.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 
 const Button = (props: any) => {
     return (
-      <button className="bg-indigo-600 text-white font-[Poppins] py-2 px-6 rounded md:ml-8 hover:bg-indigo-400 duration-500">
+      <button className="bg-green-600 text-white font-semibold font-[Poppins] py-2 px-6 rounded hover:bg-green-400 duration-500 w-52">
         {props.children}
       </button>
     );
