316db9f72b364dedbb688b6d356e9cde9d6b1fc9
jmlinger <jmlinger10@gmail.com>
2023-05-04 15:05:15 -0300

    feat: it opens in another tab when is clicked.

--- src/pages/Whatsappicon.tsx
+++ src/pages/Whatsappicon.tsx
@@ -5,7 +5,7 @@ import whatsIcon from './assets/whatsApp.svg.png'
 const WhatsappIcon = () => {
     return (
       <div className="rounded-full hover:scale-110 text-green-500 fixed bottom-0 right-0 cursor-pointer hover:text-green-700 duration-500 p-2">
-        <a href='https://wa.me/5512997482555'>
+        <a href='https://wa.me/5512997482555' target="_blank">
           <Image 
             className='w-12 h-12 md:w-20 md:h-20'
             src={whatsIcon}
