949a3b5b6853c47a267a4a7c2cc88a6ddb56ae2b
NataliaSRiber <nataliaribeiro@unifei.edu.br>
2023-05-05 11:06:42 -0300

    feat: it improves footer design

--- src/pages/Footer.tsx
+++ src/pages/Footer.tsx
@@ -3,20 +3,20 @@ import React from 'react';
 
 const Footer = (props: any) => {
     return (
-      <footer className="bg-blue-950 text-white font-[Poppins] py-2 px-6 w-full">
-        <div className='flex flex-row place-content-evenly p-2 pb-10'>
-          <div className='text-justify'>
-            <h2 className='md:text-xl font-bold pb-2 text-center'>Horários</h2>
-            <p>Segunda, quarta e sexta:</p>
-            <p>19:00 - 20:00</p>
+      <footer className="bg-blue-950 text-white font-[Poppins] py-2 md:px-6 w-full h-auto">
+        <div className='flex flex-row justify-around pl-2 md:pb-8 py-5 space-x-2'>
+          <div className='text-center'>
+            <h2 className='md:text-xl text-lg font-bold pb-2 text-center'>Horários</h2>
+            <p className='md:text-lg text-sm text'>Segunda, quarta e sexta:</p>
+            <p className='md:text-lg text-sm'>19:00 - 20:00</p>
           </div>
-          <div className='text-justify'>
-            <h2 className='md:text-xl font-bold pb-2 text-center'>Endereço</h2>
-            <p>Avenida Amaral Peixoto, nº710, Bairro Liberdade</p>
-            <p>Balneário Camboriú - SC</p>
+          <div className='text-center'>
+            <h2 className='md:text-xl text-lg font-bold pb-2 text-center'>Endereço</h2>
+            <p className='md:text-lg text-sm'>Avenida Amaral Peixoto, nº710, Bairro Liberdade</p>
+            <p className='md:text-lg text-sm'>Balneário Camboriú - SC</p>
           </div>
         </div>
-        <p className='text-center text-xs'>Site desenvolvido por @NJLinger 2023, todos os direitos reservados.</p>
+        <p className='text-center md:text-xs text-[10px]'>Site desenvolvido por @NJLinger 2023, todos os direitos reservados.</p>
       </footer>
     );
 }
