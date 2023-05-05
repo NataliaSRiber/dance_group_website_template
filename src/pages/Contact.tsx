36943cc9f874ecc1ca9a8988a36b44c96f95c207
NataliaSRiber <nataliaribeiro@unifei.edu.br>
2023-05-05 11:06:26 -0300

    feat: it modifies contact pages to improve responsive

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -11,7 +11,7 @@ const Contact = (props: any) => {
     return (
       <>
       <Nav />
-      <section className='flex flex-col py-20 md:px-20 px-10'>
+      <section className='flex flex-col md:py-32 pt-32 pb-16 md:px-20 px-10'>
       <h1 className='text-3xl md:text-5xl text-gray-700 font-semibold text-center pb-10'>Contato</h1>
         {/* <div className='border border-y-blue-900 py-14 border-x-white'>
           <h1 className='text-3xl md:text-5xl text-gray-700 font-semibold text-center pb-20'>Contato</h1>
@@ -35,7 +35,7 @@ const Contact = (props: any) => {
           </div>
         </div> */}
         <div className='pt-8 text-xl text-left'>
-          <h1 className='z md:pb-8 text-xl md:text-2xl pb-8'>Diretoria e coordenações</h1>
+          <h1 className='z md:pb-8 text-xl md:text-2xl pb-8 font-semibold text-center'>Diretoria e coordenações</h1>
           <div>
             <p className='font-medium text-lg md:text-lg pb-2'>Joaquina Silva</p>
             <p className='font-light text-sm md:text-lg pb-2'>Presidente</p>
