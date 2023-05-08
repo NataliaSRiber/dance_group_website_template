266923ea463a050a6827ef690343bf42599cffac
jmlinger <jmlinger10@gmail.com>
2023-05-08 14:15:52 -0300

    feat: it changes react slick settings and style.

--- src/pages/OurPhotos.tsx
+++ src/pages/OurPhotos.tsx
@@ -2,31 +2,33 @@ import React from 'react';
 import Image from 'next/image'
 import Nav from './Nav';
 import Slider from 'react-slick';
-// import dance1 from './assets/dance-1.jpg'
-// import dance2 from './assets/dance-2.jpg'
-// import dance3 from './assets/dance-3.jpg'
+import dance1 from './assets/dance-1.jpg'
+import dance2 from './assets/dance-2.jpg'
+import dance3 from './assets/dance-3.jpg'
 import b1 from './assets/yoga.jpg'
-import b2 from './assets/yoga.jpg'
 import na from './assets/next.png'
 import pa from './assets/back.png'
 import WhatsappIcon from './Whatsappicon';
-import styles from '../styles/banner.module.css'
 import Footer from './Footer';
-
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
+import styles from '../styles/banner.module.css'
 
 const NextBtn = (props: any) => {
-  const { onClick } = props;
+  const { onClick, visibility } = props;
+  console.log(visibility);
+  
   return (
-    <div className='text-blue-950 md:w-64 cursor-pointer' onClick={onClick}>
+    <div className={`${visibility} text-blue-950 cursor-pointer`} onClick={onClick}>
       <Image src={na} alt="next-arrow"/>
     </div>
   );
 };
 
 const PrevBtn = (props: any) => {
-  const { onClick } = props;
+  const { onClick, visibility } = props;
   return (
-    <div className='text-blue-950 md:w-64 cursor-pointer' onClick={onClick}>
+    <div className={`${visibility} text-blue-950 cursor-pointer`} onClick={onClick}>
       <Image src={pa} alt="prev-arrow" />
     </div>
   );
@@ -34,32 +36,34 @@ const PrevBtn = (props: any) => {
 
 const OurPhotos = () => {
   const settings = {
-    // infinite: true,
-    className: "center",
-    centerMode: true,
+    infinite: true,
+    // centerMode: true,
     slidesToShow: 3,
     slidesToScroll: 1,
     speed: 3000,
     autoplay: true,
-    autoplaySpeed: 500,
-    nextArrow: <NextBtn />,
-    prevArrow: <PrevBtn />,
+    autoplaySpeed: 6000,
+    nextArrow: <NextBtn visibility={'visible'}/>,
+    prevArrow: <PrevBtn visibility={'visible'}/>,
+    pauseOnHover: true,
 
     responsive: [
       {
         breakpoint: 1024,
         settings: {
           slidesToShow: 2,
-          slidesToScroll: 2,
+          slidesToScroll: 1,
           dots: true
         }
       },
       {
-        breakpoint: 720,
+        breakpoint: 767,
         settings: {
           slidesToShow: 2,
-          slidesToScroll: 2,
-          initialSlide: 1,
+          slidesToScroll: 1,
+          nextArrow: <NextBtn visibility={'invisible'}/>,
+          prevArrow: <PrevBtn visibility={'invisible'}/>,
+          dots: true
         }
       },
       {
@@ -67,6 +71,9 @@ const OurPhotos = () => {
         settings: {
           slidesToShow: 1,
           slidesToScroll: 1,
+          nextArrow: <NextBtn visibility={'invisible'}/>,
+          prevArrow: <PrevBtn visibility={'invisible'}/>,
+          // dots: true
         }
       }
     ]
@@ -75,245 +82,245 @@ const OurPhotos = () => {
       <>
         <Nav />
         <h1 className='text-3xl md:text-5xl text-gray-700 font-semibold text-center md:pt-32 pt-32 pb-16 md:px-20 px-10'>Galeria de fotos</h1>
-        <div>
-          <Slider {...settings} className={`${styles["slick-slider"]} pt-4 mx-5`}>
+        <div className={`${styles.sliderContainer} w-full`}>
+          <Slider {...settings} className='md:pt-20 pt-10 md:mb-20 mb-10'>
             <div>
               <Image
                   className='w-{100}'
-                  src={b2}
+                  src={b1}
                   alt="Picture of the author"
                 />
             </div>
             <div>
               <Image
                   className='w-{100}'
-                  src={b2}
+                  src={b1}
                   alt="Picture of the author"
                 />
             </div>
             <div>
               <Image
                   className='w-{100}'
-                  src={b2}
+                  src={b1}
                   alt="Picture of the author"
                 />
             </div>
             <div>
               <Image
                   className='w-{100}'
-                  src={b2}
+                  src={b1}
                   alt="Picture of the author"
                 />
             </div>
             <div>
               <Image
                   className='w-{100}'
-                  src={b2}
+                  src={b1}
                   alt="Picture of the author"
                 />
             </div>
             <div>
               <Image
                   className='w-{100}'
-                  src={b2}
+                  src={b1}
                   alt="Picture of the author"
                 />
             </div>
             <div>
               <Image
                   className='w-{100}'
-                  src={b2}
+                  src={b1}
                   alt="Picture of the author"
                 />
             </div>
             <div>
               <Image
                   className='w-{100}'
-                  src={b2}
+                  src={b1}
                   alt="Picture of the author"
                 />
             </div>
             <div>
               <Image
                   className='w-{100}'
-                  src={b2}
+                  src={b1}
                   alt="Picture of the author"
                 />
             </div>
             <div>
               <Image
                   className='w-{100}'
-                  src={b2}
+                  src={b1}
                   alt="Picture of the author"
                 />
             </div>
             <div>
               <Image
                   className='w-{100}'
-                  src={b2}
+                  src={b1}
                   alt="Picture of the author"
                 />
             </div>
             <div>
               <Image
                   className='w-{100}'
-                  src={b2}
+                  src={b1}
                   alt="Picture of the author"
                 />
             </div>
             <div>
               <Image
                   className='w-{100}'
-                  src={b2}
+                  src={b1}
                   alt="Picture of the author"
                 />
             </div>
             <div>
               <Image
                   className='w-{100}'
-                  src={b2}
+                  src={b1}
                   alt="Picture of the author"
                 />
             </div>
             <div>
               <Image
                   className='w-{100}'
-                  src={b2}
+                  src={b1}
                   alt="Picture of the author"
                 />
             </div>
             <div>
               <Image
                   className='w-{100}'
-                  src={b2}
+                  src={b1}
                   alt="Picture of the author"
                 />
             </div>
             <div>
               <Image
                   className='w-{100}'
-                  src={b2}
+                  src={b1}
                   alt="Picture of the author"
                 />
             </div>
             <div>
               <Image
                   className='w-{100}'
-                  src={b2}
+                  src={b1}
                   alt="Picture of the author"
                 />
             </div>    
           </Slider>
-          <Slider {...settings} className={`${styles["slick-slider"]} md:pt-20 pt-10 md:pb-20 pb-10`}>
+          <Slider {...settings} className='md:pt-20 pt-10 md:mb-20 mb-10'>
             <div>
               <Image
                   className='w-auto'
-                  src={b1}
+                  src={dance1}
                   alt="Picture of the author"
                 />
             </div>
             <div>
               <Image
                   className='w-{100}'
-                  src={b1}
+                  src={dance2}
                   alt="Picture of the author"
                 />
             </div>
             <div>
               <Image
                   className='w-{100}'
-                  src={b1}
+                  src={dance3}
                   alt="Picture of the author"
                 />
             </div>
             <div>
               <Image
                   className='w-{100}'
-                  src={b1}
+                  src={dance1}
                   alt="Picture of the author"
                 />
             </div>
             <div>
               <Image
                   className='w-{100}'
-                  src={b1}
+                  src={dance2}
                   alt="Picture of the author"
                 />
             </div>
             <div>
               <Image
                   className='w-{100}'
-                  src={b1}
+                  src={dance3}
                   alt="Picture of the author"
                 />
             </div>
             <div>
               <Image
                   className='w-{100}'
-                  src={b1}
+                  src={dance1}
                   alt="Picture of the author"
                 />
             </div>
             <div>
               <Image
                   className='w-{100}'
-                  src={b1}
+                  src={dance2}
                   alt="Picture of the author"
                 />
             </div>
             <div>
               <Image
                   className='w-{100}'
-                  src={b1}
+                  src={dance3}
                   alt="Picture of the author"
                 />
             </div>
             <div>
               <Image
                   className='w-{100}'
-                  src={b1}
+                  src={dance1}
                   alt="Picture of the author"
                 />
             </div>
             <div>
               <Image
                   className='w-{100}'
-                  src={b1}
+                  src={dance2}
                   alt="Picture of the author"
                 />
             </div>
             <div>
               <Image
                   className='w-{100}'
-                  src={b1}
+                  src={dance3}
                   alt="Picture of the author"
                 />
             </div>
             <div>
               <Image
                   className='w-{100}'
-                  src={b1}
+                  src={dance1}
                   alt="Picture of the author"
                 />
             </div>
             <div>
               <Image
                   className='w-{100}'
-                  src={b1}
+                  src={dance2}
                   alt="Picture of the author"
                 />
             </div>
             <div>
               <Image
                   className='w-{100}'
-                  src={b1}
+                  src={dance3}
                   alt="Picture of the author"
                 />
             </div>
             <div>
               <Image
                   className='w-{100}'
-                  src={b1}
+                  src={dance1}
                   alt="Picture of the author"
                 />
             </div>
