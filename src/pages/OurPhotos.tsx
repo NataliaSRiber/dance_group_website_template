b9b52a3723f83bd72f0e38dfd2c407997e1220f6
jmlinger <jmlinger10@gmail.com>
2023-05-09 10:56:37 -0300

    feat: it changes react slick settings and style.

--- src/pages/OurPhotos.tsx
+++ src/pages/OurPhotos.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Image from 'next/image'
 import Nav from './Nav';
 import Slider from 'react-slick';
@@ -6,21 +6,21 @@ import dance1 from './assets/dance-1.jpg'
 import dance2 from './assets/dance-2.jpg'
 import dance3 from './assets/dance-3.jpg'
 import b1 from './assets/yoga.jpg'
-import na from './assets/next.png'
-import pa from './assets/back.png'
 import WhatsappIcon from './Whatsappicon';
 import Footer from './Footer';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
 import styles from '../styles/banner.module.css'
+import { IoIosArrowForward, IoIosArrowBack } from 'react-icons/io';
+
+const images = [dance1, dance2, dance3, dance1, dance2, dance3, dance1, dance2, dance3] 
 
 const NextBtn = (props: any) => {
   const { onClick, visibility } = props;
-  console.log(visibility);
   
   return (
-    <div className={`${visibility} text-blue-950 cursor-pointer`} onClick={onClick}>
-      <Image src={na} alt="next-arrow"/>
+    <div className={`${visibility} cursor-pointer`} onClick={onClick}>
+      <IoIosArrowForward className="text-3xl md:text-5xl text-blue-950 hover:text-blue-700 duration-300"/>
     </div>
   );
 };
@@ -28,8 +28,8 @@ const NextBtn = (props: any) => {
 const PrevBtn = (props: any) => {
   const { onClick, visibility } = props;
   return (
-    <div className={`${visibility} text-blue-950 cursor-pointer`} onClick={onClick}>
-      <Image src={pa} alt="prev-arrow" />
+    <div className={`${visibility} cursor-pointer `} onClick={onClick}>
+      <IoIosArrowBack className="text-3xl md:text-5xl text-blue-950 hover:text-blue-700 duration-300"/>
     </div>
   );
 };
@@ -37,29 +37,31 @@ const PrevBtn = (props: any) => {
 const OurPhotos = () => {
   const settings = {
     infinite: true,
-    // centerMode: true,
+    centerMode: true,
+    centerPadding: "0px",
+    speed: 2000,
     slidesToShow: 3,
     slidesToScroll: 1,
-    speed: 3000,
-    autoplay: true,
-    autoplaySpeed: 6000,
+    // autoplay: true,
+    autoplaySpeed: 5000,
     nextArrow: <NextBtn visibility={'visible'}/>,
     prevArrow: <PrevBtn visibility={'visible'}/>,
     pauseOnHover: true,
+    swipeToSlide: true,
 
     responsive: [
       {
         breakpoint: 1024,
         settings: {
-          slidesToShow: 2,
+          slidesToShow: 3,
           slidesToScroll: 1,
           dots: true
         }
       },
       {
-        breakpoint: 767,
+        breakpoint: 768,
         settings: {
-          slidesToShow: 2,
+          slidesToShow: 1,
           slidesToScroll: 1,
           nextArrow: <NextBtn visibility={'invisible'}/>,
           prevArrow: <PrevBtn visibility={'invisible'}/>,
@@ -84,260 +86,37 @@ const OurPhotos = () => {
         <h1 className='text-3xl md:text-5xl text-gray-700 font-semibold text-center md:pt-32 pt-32 pb-16 md:px-20 px-10'>Galeria de fotos</h1>
         <div className={`${styles.sliderContainer} w-full`}>
           <Slider {...settings} className='md:pt-20 pt-10 md:mb-20 mb-10'>
-            <div>
-              <Image
-                  className='w-{100}'
-                  src={b1}
-                  alt="Picture of the author"
-                />
-            </div>
-            <div>
-              <Image
-                  className='w-{100}'
-                  src={b1}
-                  alt="Picture of the author"
-                />
-            </div>
-            <div>
-              <Image
-                  className='w-{100}'
-                  src={b1}
-                  alt="Picture of the author"
-                />
-            </div>
-            <div>
-              <Image
-                  className='w-{100}'
-                  src={b1}
-                  alt="Picture of the author"
-                />
-            </div>
-            <div>
-              <Image
-                  className='w-{100}'
-                  src={b1}
-                  alt="Picture of the author"
-                />
-            </div>
-            <div>
-              <Image
-                  className='w-{100}'
-                  src={b1}
-                  alt="Picture of the author"
-                />
-            </div>
-            <div>
-              <Image
-                  className='w-{100}'
-                  src={b1}
-                  alt="Picture of the author"
-                />
-            </div>
-            <div>
-              <Image
-                  className='w-{100}'
-                  src={b1}
-                  alt="Picture of the author"
-                />
-            </div>
-            <div>
-              <Image
-                  className='w-{100}'
-                  src={b1}
-                  alt="Picture of the author"
-                />
-            </div>
-            <div>
-              <Image
-                  className='w-{100}'
-                  src={b1}
-                  alt="Picture of the author"
-                />
-            </div>
-            <div>
-              <Image
-                  className='w-{100}'
-                  src={b1}
-                  alt="Picture of the author"
-                />
-            </div>
-            <div>
-              <Image
-                  className='w-{100}'
-                  src={b1}
-                  alt="Picture of the author"
-                />
-            </div>
-            <div>
-              <Image
-                  className='w-{100}'
-                  src={b1}
-                  alt="Picture of the author"
-                />
-            </div>
-            <div>
-              <Image
-                  className='w-{100}'
-                  src={b1}
-                  alt="Picture of the author"
-                />
-            </div>
-            <div>
-              <Image
-                  className='w-{100}'
-                  src={b1}
-                  alt="Picture of the author"
-                />
-            </div>
-            <div>
-              <Image
-                  className='w-{100}'
-                  src={b1}
-                  alt="Picture of the author"
-                />
-            </div>
-            <div>
-              <Image
-                  className='w-{100}'
-                  src={b1}
-                  alt="Picture of the author"
-                />
-            </div>
-            <div>
-              <Image
-                  className='w-{100}'
-                  src={b1}
-                  alt="Picture of the author"
-                />
-            </div>    
+            {images.map((img) =>(
+              <div>
+                <Image
+                    className=''
+                    src={img}
+                    alt="Picture of the author"
+                  />
+              </div>
+            ))}
           </Slider>
-          <Slider {...settings} className='md:pt-20 pt-10 md:mb-20 mb-10'>
-            <div>
-              <Image
-                  className='w-auto'
-                  src={dance1}
-                  alt="Picture of the author"
-                />
-            </div>
-            <div>
-              <Image
-                  className='w-{100}'
-                  src={dance2}
-                  alt="Picture of the author"
-                />
-            </div>
-            <div>
-              <Image
-                  className='w-{100}'
-                  src={dance3}
-                  alt="Picture of the author"
-                />
-            </div>
-            <div>
-              <Image
-                  className='w-{100}'
-                  src={dance1}
-                  alt="Picture of the author"
-                />
-            </div>
-            <div>
-              <Image
-                  className='w-{100}'
-                  src={dance2}
-                  alt="Picture of the author"
-                />
-            </div>
-            <div>
-              <Image
-                  className='w-{100}'
-                  src={dance3}
-                  alt="Picture of the author"
-                />
-            </div>
-            <div>
-              <Image
-                  className='w-{100}'
-                  src={dance1}
-                  alt="Picture of the author"
-                />
-            </div>
-            <div>
-              <Image
-                  className='w-{100}'
-                  src={dance2}
-                  alt="Picture of the author"
-                />
-            </div>
-            <div>
-              <Image
-                  className='w-{100}'
-                  src={dance3}
-                  alt="Picture of the author"
-                />
-            </div>
-            <div>
-              <Image
-                  className='w-{100}'
-                  src={dance1}
-                  alt="Picture of the author"
-                />
-            </div>
-            <div>
-              <Image
-                  className='w-{100}'
-                  src={dance2}
-                  alt="Picture of the author"
-                />
-            </div>
-            <div>
-              <Image
-                  className='w-{100}'
-                  src={dance3}
-                  alt="Picture of the author"
-                />
-            </div>
-            <div>
-              <Image
-                  className='w-{100}'
-                  src={dance1}
-                  alt="Picture of the author"
-                />
-            </div>
-            <div>
-              <Image
-                  className='w-{100}'
-                  src={dance2}
-                  alt="Picture of the author"
-                />
-            </div>
-            <div>
-              <Image
-                  className='w-{100}'
-                  src={dance3}
-                  alt="Picture of the author"
-                />
-            </div>
-            <div>
-              <Image
-                  className='w-{100}'
-                  src={dance1}
-                  alt="Picture of the author"
-                />
-            </div>
-            <div>
-              <Image
-                  className='w-{100}'
-                  src={b1}
-                  alt="Picture of the author"
-                />
-            </div>
-            <div>
-              <Image
-                  className='w-{100}'
-                  src={b1}
-                  alt="Picture of the author"
-                />
-            </div>    
+          <Slider {...settings} className=''>
+            {images.map((img) =>(
+                <div>
+                  <Image
+                      className=''
+                      src={img}
+                      alt="Picture of the author"
+                    />
+                </div>
+            ))}
+          </Slider>
+          <Slider {...settings} className=''>
+            {images.map((img) =>(
+                <div>
+                  <Image
+                      className=''
+                      src={img}
+                      alt="Picture of the author"
+                    />
+                </div>
+            ))}
           </Slider>
         </div>
         <WhatsappIcon />
