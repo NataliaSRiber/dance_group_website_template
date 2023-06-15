e7a7a259b2893c8ee432b4af4ba5c4eef8f6ea43
jmlinger <jmlinger10@gmail.com>
2023-06-15 15:11:46 -0300

    chore: it changes some config files.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/**/*.{js,ts,jsx,tsx}",
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
       fontFamily: {
-        instrument: ['Instrument Serif', 'serif']
+        instrument: ['Instrument Serif', 'serif'],
       },
     },
   },
   plugins: [],
 }
-
