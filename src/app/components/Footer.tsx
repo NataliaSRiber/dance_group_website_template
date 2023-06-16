52447134138dc09437500e12daf5d13c35f3bb31
NataliaSRiber <nataliaribeiro@unifei.edu.br>
2023-06-16 17:10:20 -0300

    refactor: it modifies footer size

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -4,21 +4,21 @@ export default function Footer() {
   return (
     <>
       <footer className="h-auto w-full bg-blue-950 py-2 text-white md:px-6">
-        <div className="flex flex-row justify-around space-x-2 py-2 pl-2 md:pb-2">
+        <div className="flex md:flex-row justify-around space-x-2 py-2 pl-2 md:pb-2 flex-col">
           <div className="text-center">
             <h2 className="pb-2 text-center text-lg font-bold md:text-xl">
               Horários
             </h2>
             <p className="text text-sm md:text-lg">Segunda, Quarta e Sexta:</p>
-            <p className="md:text-md text-sm">9:00 - 10:00</p>
-            <p className="md:text-md text-sm">19:00 - 20:00</p>
+            <p className="md:text-base text-sm">9:00 - 10:00</p>
+            <p className="md:text-base text-sm">19:00 - 20:00</p>
           </div>
           <div className="text-center">
-            <h2 className="pb-2 text-center text-lg font-bold md:text-xl">
+            <h2 className="pb-2 pt-4 text-center text-lg font-bold md:text-xl">
               Endereço
             </h2>
-            <p className="text-sm md:text-lg">
-              Avenida Amaral Peixoto, nº710, Bairro Liberdade
+            <p className="text-sm md:text-base">
+              Av. Amaral Peixoto, nº0, Bairro Liberdade
             </p>
             <p className="text-sm md:text-lg">Esperança - SP</p>
           </div>
