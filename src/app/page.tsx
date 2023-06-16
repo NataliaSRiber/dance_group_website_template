68d2ff41727bb43ae6612bd49a1bd1643c7a6d29
NataliaSRiber <nataliaribeiro@unifei.edu.br>
2023-06-16 17:03:41 -0300

    feat: it creates about page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,7 @@ import React, { useEffect, useState } from 'react'
 import Image from 'next/image'
 import women from '../assets/main-photo.jpg'
 import Nav from './components/Nav'
+import Link from 'next/link'
 
 export default function Home() {
   const [animation, setAnimation] = useState(false)
@@ -30,7 +31,7 @@ export default function Home() {
             className={`${
               animation &&
               'translate-x-full transform transition delay-1000 duration-[900ms] ease-in'
-            } text-md relative left-[-100%] font-semibold tracking-wide md:pb-2 md:text-4xl`}
+            } text-md relative left-[-100%] font-bold tracking-wide md:text-5xl pb-4 md:pb-8`}
           >
             Grupo de Dança
           </h1>
@@ -38,15 +39,23 @@ export default function Home() {
             className={`${
               animation &&
               'translate-x-[-100%] transform transition delay-1000 duration-[900ms] ease-in'
-            } relative right-[-100%] text-xs font-light md:text-xl`}
+            } relative right-[-100%] text-xs font-light md:text-xl md:pb-20 pb-6`}
           >
-            Grupo de dança de Esperança, São Paulo
+            Venha aprender a dançar conosco!
           </p>
+          <div className={`scale-0 flex items-center justify-center ${animation && 'scale-100 transform transition delay-[2000ms] duration-500'}`}>
+            <Link href="/contact" >
+              <button className="drop-shadow-xl flex cursor-pointer items-center justify-between rounded-lg bg-blue-950 md:p-4 md:text-lg p-2 text-xs font-bold text-white hover:bg-blue-400 relative">
+                Junte-se a nós
+              </button>
+            </Link>
+
+          </div>
         </div>
       </div>
       <div className="mx-auto flex flex-col px-10 py-10 md:px-20 md:py-20">
-        <h1 className="pb-10 text-center text-3xl font-semibold text-gray-700 md:pb-20 md:text-5xl">
-          Sobre nós
+        <h1 className="pb-10 text-center text-2xl font-semibold text-gray-700 md:pb-20 md:text-5xl">
+          Seja bem vindo ao Grupo de Dança
         </h1>
         <p className="text-md text-justify md:text-xl">
           Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec
@@ -56,50 +65,7 @@ export default function Home() {
           venenatis, eu fermentum lorem placerat. Nunc viverra lobortis mauris,
           id rhoncus elit rutrum quis. Praesent feugiat finibus enim, sed tempor
           tortor dictum in. Sed eros magna, pulvinar id mollis in, ornare ac
-          nunc. Donec non sagittis felis. Fusce congue quam velit. Vivamus non
-          elit risus. Duis ante nibh, porttitor in ipsum a, luctus dapibus
-          lectus. Praesent facilisis, felis et viverra vehicula, metus turpis
-          accumsan velit, ut sollicitudin risus mauris non nisl. Mauris mattis
-          lobortis mauris ut varius. Lorem ipsum dolor sit amet, consectetur
-          adipiscing elit. Praesent at vehicula est. Proin egestas, nisl eu
-          semper aliquet, libero quam aliquam nunc, nec rhoncus mi nibh ut nunc.
-          Curabitur consectetur, erat eu euismod eleifend, mauris nibh feugiat
-          eros, et dignissim lacus augue non ante. Suspendisse venenatis
-          consectetur sem vel mattis. Nunc eget sapien vitae mauris vulputate
-          volutpat quis sed erat. Pellentesque habitant morbi tristique senectus
-          et netus et malesuada fames ac turpis egestas. Etiam sollicitudin leo
-          neque, vel vulputate mi sagittis a. Cras eget sapien iaculis, egestas
-          leo eget, suscipit risus. Aliquam auctor leo non imperdiet
-          ullamcorper. Curabitur ac justo eget ex dictum vehicula. Ut tristique
-          consequat magna ut convallis. Donec et pharetra ipsum. Integer non
-          quam facilisis, feugiat sapien non, varius justo. Phasellus in mi vel
-          ante tristique dignissim sit amet sed ligula. Quisque et dapibus quam.
-          Vestibulum ante ipsum primis in faucibus orci luctus et ultrices
-          posuere cubilia curae; Nam volutpat varius massa, eu malesuada justo
-          facilisis et. Nulla a nulla libero. Donec non consequat tortor. Proin
-          tempor, augue a vestibulum egestas, elit velit pharetra diam, vitae
-          auctor arcu dolor faucibus magna. Duis vel vulputate elit. Sed
-          volutpat lectus sit amet dolor pharetra sollicitudin id id tellus.
-          Pellentesque habitant morbi tristique senectus et netus et malesuada
-          fames ac turpis egestas. In hac habitasse platea dictumst. Ut eleifend
-          blandit dolor, fermentum pretium ipsum maximus quis. Aenean pulvinar
-          elit ante, vehicula iaculis diam fermentum vulputate. Sed euismod,
-          mauris id eleifend aliquet, ipsum leo vulputate nulla, sit amet
-          sodales urna nisi vel eros. Suspendisse aliquet arcu pretium egestas
-          ullamcorper. Etiam sed imperdiet tortor, eu molestie enim. Etiam
-          dapibus interdum neque, nec laoreet dolor varius sed. Aenean dui
-          lacus, fermentum ut nibh eget, efficitur tincidunt mi. Duis tincidunt
-          ac orci vel imperdiet. Etiam et leo at diam porttitor vehicula. Nulla
-          eget elit ultrices, aliquet tortor ut, convallis purus. Etiam molestie
-          tellus sit amet ex facilisis, vel eleifend metus placerat. Etiam
-          hendrerit ante dui, in finibus diam sollicitudin ac. Fusce lobortis
-          orci ligula, quis molestie arcu hendrerit nec. Duis eu nulla vel mi
-          fringilla molestie vel id massa. Curabitur ut malesuada dui. Duis
-          pretium lacus id dui sagittis pretium. Ut accumsan quam a risus
-          tempor, interdum pellentesque erat pulvinar. Nunc massa neque, mollis
-          in congue id, scelerisque at ipsum. Nam aliquam tellus non ligula
-          mollis cursus. In vel molestie nulla, non mattis turpis. Nunc sed urna
-          ac lacus ultricies aliquam sodales nec massa.
+          nunc. Donec non sagittis felis. Fusce congue quam velit. 
         </p>
       </div>
     </>
