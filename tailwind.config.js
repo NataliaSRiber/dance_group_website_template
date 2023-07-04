21f3948ee9f6e139dca8bcc834a255d9561355ed
jmlinger <jmlinger10@gmail.com>
2023-07-04 11:39:34 -0300

    build: it adds new screen sizes.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,7 @@
 /** @type {import('tailwindcss').Config} */
+
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 module.exports = {
   content: [
     './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
@@ -6,12 +9,10 @@ module.exports = {
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
-    extend: {
-      // backgroundImage: {
-      //   'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-      //   'gradient-conic':
-      //     'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      // },
+    screens: {
+      '2xs': '384px',
+      xs: '512px',
+      ...defaultTheme.screens,
     },
   },
   plugins: [],
